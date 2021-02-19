a89eedcd603d28d55f035093465bd79ed71aca43
Ana Carolina <cscarol20@gmail.com>
2021-02-19 10:56:17 -0300

    Initial Commit

---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Ana Carolina on 19/02/21.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
