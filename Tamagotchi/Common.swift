07c984c72a7b8083a1f30888bed91e668ea4aa3e
Ana Carolina <cscarol20@gmail.com>
2021-02-20 18:30:04 -0300

    Add Widget to the app

---
+++ Tamagotchi/Common.swift
@@ -0,0 +1,37 @@
+//
+//  Common.swift
+//  Tamagotchi
+//
+//  Created by Ana Carolina on 20/02/21.
+//
+
+import Foundation
+import SwiftUI
+
+// Extensions for colors
+extension Color {
+    
+    static let alternativeBlack = Color(UIColor(named: "black")!)
+    static let colorui = Color(UIColor(named: "colorui")!)
+    static let darkBlue = Color(UIColor(named: "bgdarkblue")!)
+    static let cleanBlue = Color(UIColor(named: "bgcleanblue")!)
+    static let bgTop = Color(UIColor(named: "bgTop")!)
+    static let bgBottom = Color(UIColor(named: "bgBottom")!)
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
